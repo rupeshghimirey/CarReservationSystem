537c8d700c03d8b9bf56b86f04546bb757f7ea1f
Chongwei Ma <chongwei.ma@outlook.com>
2022-11-14 08:07:30 -0800

    # removed map

---
+++ src/test/com/crs/datahub/CarInventoryTest.java
@@ -0,0 +1,12 @@
+package com.crs.datahub;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CarInventoryTest {
+
+    @Test
+    public void carInventoryInitialization() {
+    }
+}
