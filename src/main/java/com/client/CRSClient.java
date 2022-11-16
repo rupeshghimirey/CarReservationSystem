ad214500379e6cc06e14a37f0fefa4b2de9c63e9
Rupesh Ghimire <rupesh.ghimire@tlgcohort.com>
2022-11-16 16:30:18 -0500

    updated inventory and refactored CRSClient

--- src/main/java/com/client/CRSClient.java
+++ src/main/java/com/client/CRSClient.java
@@ -11,46 +11,48 @@ import java.io.FileNotFoundException;
 import java.util.stream.Collectors;
 
 public class CRSClient {
+    private static final String MAIN_OPTION_1 = "1";
+    private static final String MAIN_OPTION_2 = "2";
+    private static final String MAIN_OPTION_3 = "2";
+    private static final String SUB_OPTION_1 = "1";
+    private static final String SUB_OPTION_2 = "2";
+    private static final String SUB_OPTION_3 = "3";
+    private static final String SUB_OPTION_4 = "4";
     public static void main(String[] args) throws FileNotFoundException {
         GlobalVariable.userInterface = new UserInterface();
         GlobalVariable.invoiceReservation = new InvoiceReservation();
 
-        Customer customer1 = new Customer("1", "Rupesh", "Ghimire", "123-456-7891",
-                new Address("86 Boston Hbr", "Cameron", "NC", "28326"),
-                new CreditCard("Rupesh", "1111 2222 3333 4444", "222", "10/45") {});
-
         while (GlobalVariable.isMenuRunning) {
             String userInput = GlobalVariable.userInterface.printMainMenu();
 
-            if (userInput.equalsIgnoreCase("1")) {
+            if (userInput.equalsIgnoreCase(MAIN_OPTION_1)) {
                 MainMenuOption1.onClick1();
-            } else if (userInput.equalsIgnoreCase("2")) {
+            } else if (userInput.equalsIgnoreCase(MAIN_OPTION_2)) {
                 String subMenu2Choice = GlobalVariable.userInterface.printSubMenuOfTwo();
                 GlobalVariable.isSubMenu2Choice = true;
 
                 while (GlobalVariable.isSubMenu2Choice) {
-                    if (subMenu2Choice.equals("1")) {
-                        SubMenu2Choice1.subMenu2Choice1(customer1);
+                    if (subMenu2Choice.equals(SUB_OPTION_1)) {
+                        SubMenu2Choice1.subMenu2Choice1(GlobalVariable.userInterface.getCustomerInfo());
 
                         GlobalVariable.isSubMenu2Choice = false;
-                    } else if (subMenu2Choice.equals("2")) {
+                    } else if (subMenu2Choice.equals(SUB_OPTION_2)) {
 
-                        SubMenu2Choice2.subMenu2Choice2(customer1);
-
-                        GlobalVariable.isSubMenu2Choice = false;
-
-                    } else if (subMenu2Choice.equals("3")) {
-                        SubMenu2Choice3.subMenu2Choice3(customer1);
+                        SubMenu2Choice2.subMenu2Choice2(GlobalVariable.userInterface.getCustomerInfo());
 
                         GlobalVariable.isSubMenu2Choice = false;
-                    } else if (subMenu2Choice.equals("4")) {
 
-                        System.out.println("You have selected FOUR!");
+                    } else if (subMenu2Choice.equals(SUB_OPTION_3)) {
+                        SubMenu2Choice3.subMenu2Choice3(GlobalVariable.userInterface.getCustomerInfo());
+
+                        GlobalVariable.isSubMenu2Choice = false;
+                    } else if (subMenu2Choice.equals(SUB_OPTION_4)) {
+
                         GlobalVariable.isSubMenu2Choice = false;
                     }
                 }
-            } else if (userInput.equalsIgnoreCase("3")) {
-                System.out.println("Thank you for visiting CRS!");
+            } else if (userInput.equalsIgnoreCase(MAIN_OPTION_3)) {
+                GlobalVariable.userInterface.thankYouMessage();
 
                 GlobalVariable.invoiceReservation.closeFile();//may not be necessary
 
