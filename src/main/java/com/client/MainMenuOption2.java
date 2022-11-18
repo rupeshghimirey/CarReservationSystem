c7428591301dcd738c468ee010afffa19827ba7c
12TheHangedMan/TLGCohort <106269882+12TheHangedMan@users.noreply.github.com>
2022-11-18 12:48:08 -0800

    Update MainMenuOption2.java

--- src/main/java/com/client/MainMenuOption2.java
+++ src/main/java/com/client/MainMenuOption2.java
@@ -3,29 +3,23 @@ package com.client;
 import static com.client.GlobalVariable.*;
 
 public class MainMenuOption2 {
-    public static void mainMenuOption2() {
-        String subMenu2Choice = userInterface.printSubMenuTwo();
-        isSubMenu2Choice = true;
-
+    public static void mainMenuOption2() {    
         while (isSubMenu2Choice) {
+            String subMenu2Choice = userInterface.printSubMenuTwo();
+            
             if (subMenu2Choice.equals(SUB_OPTION_1)) {
 
                 SubMenu2Choice1.subMenu2Choice1();
 
-                isSubMenu2Choice = false;
             } else if (subMenu2Choice.equals(SUB_OPTION_2)) {
 
                 SubMenu2Choice2.subMenu2Choice2();
 
-                isSubMenu2Choice = false;
-
             } else if (subMenu2Choice.equals(SUB_OPTION_3)) {
+                
                 SubMenu2Choice3.subMenu2Choice3();
-
-                isSubMenu2Choice = false;
+                
             } else if (subMenu2Choice.equals(SUB_OPTION_4)) {
-
-                isSubMenu2Choice = false;
                 break;
             }
         }
